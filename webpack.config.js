6dea0a098753dccc8fcd1d2dd20fcf1754b3ec1e
netgabo <netgabo@gmail.com>
2021-04-16 16:43:39 -0300

    Subida

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+	mode: 
+		'none',
+	
+	entry: [
+		'./src/main.js'
+	],
+	output: {
+		filename: 'build.js'
+	},
+	module: {
+		rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            }
+		]
+	}
+};
